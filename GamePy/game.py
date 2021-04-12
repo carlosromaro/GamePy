148746798e008c3cb29611bc46c3f2fef18730f5
carlosromaro <carlosromaro42@gmail.com>
2021-04-12 20:04:54 -0300

    commit GamePy

---
+++ GamePy/game.py
@@ -0,0 +1,64 @@
+from models.calcular import Calcular
+import PySimpleGUI as sg
+
+
+def main() -> None:
+    login()
+
+
+def login() -> None:
+    score_s: int = 5
+    attempts_s: int = 0
+
+    layout1 = [[sg.Text("Informe seu nome para jogar: "), sg.Input(key='name')],
+               [sg.Text('Informe a dificuldade desejada [1, 2, 3]: '), sg.Input(key='difficulty')],
+               [sg.Button('OK'), sg.Button('Exit')]]
+
+    window = sg.Window('Inicio', layout1)
+    event, values = window.read()
+
+    if event is None or event == 'Exit':
+        window.close()
+
+    name = values['name']
+    difficulty = int(values['difficulty'])
+
+    if name and difficulty:
+        window.close()
+        jogar(name, difficulty, score_s, attempts_s)
+
+
+def jogar(name: str, difficulty: int, score: int, attempts: int):
+    obj = Calcular(difficulty)
+    problem = obj.gerar_operacao()
+
+    layout2 = [[sg.Text(f'Jogador: {name} | Pontos Faltando: {score} | Tentativas: {attempts}')],
+               [sg.Text(f'Problema {attempts + 1}: {problem}'), sg.Input(key='answer')],
+               [sg.Text('Resposta = '), sg.Text(key='-verifica-')],
+               [sg.Button('OK'), sg.Button('Exit')]]
+
+    window = sg.Window('GAME', layout2)
+    event, values = window.read()
+
+    if event == 'Exit':
+        exit(1)
+        window.close()
+
+    test = obj.checar_resultado(int(values['answer']))
+
+    if test:
+        score -= 1
+        attempts += 1
+        window['-verifica-'].update('CORRETA')
+    else:
+        e = 'ERRADA'
+        attempts += 1
+        window['-verifica-'].update(str(e))
+
+    window.read()
+    window.close()
+    jogar(name, difficulty, score, attempts)
+
+
+if __name__ == '__main__':
+    main()
---
+++ GamePy/models/calcular.py
@@ -0,0 +1,93 @@
+from random import randint
+
+
+class Calcular:
+
+    def __init__(self: object, dificuldade: int) -> None:
+        self.__dificuldade: int = dificuldade
+        self.__operacao: int = randint(1, 3)
+        self.__valor1: int = self._gerar_valor
+        self.__valor2: int = self._gerar_valor
+        self.__resultado: int = self._gerar_resultado
+
+    @property
+    def dificuldade(self: object) -> int:
+        return self.__dificuldade
+
+    @property
+    def valor1(self: object) -> int:
+        return self.__valor1
+
+    @property
+    def valor2(self: object) -> int:
+        return self.__valor2
+
+    @property
+    def operacao(self: object) -> int:
+        return self.__operacao
+
+    @property
+    def resultado(self: object) -> int:
+        return self.__resultado
+
+    @property
+    def _gerar_valor(self: object) -> int:
+        if self.__dificuldade == 1 and (self.operacao == 1 or self.__operacao == 2):
+            return randint(5, 30)
+        elif self.__dificuldade == 2 and (self.operacao == 1 or self.__operacao == 2):
+            return randint(16, 99)
+        elif self.__dificuldade == 3 and (self.__operacao == 1 or self.__operacao == 2):
+            return randint(30, 150)
+        elif self.__dificuldade == 1 and self.__operacao == 3:
+            return randint(3, 10)
+        elif self.__dificuldade == 2 and self.__operacao == 3:
+            return randint(3, 20)
+        elif self.__dificuldade == 3 and self.__operacao == 3:
+            return randint(5, 35)
+
+    @property
+    def _gerar_resultado(self):
+        if self.__operacao == 1:
+            return self.__valor1 + self.__valor2
+        elif self.__operacao == 2:
+            return self.__valor1 - self.__valor2
+        elif self.__operacao == 3:
+            return self.__valor2 * self.__valor1
+
+    def __str__(self):
+        return f'{self.valor1}\n{self.valor2}\n{self.operacao}\n{self.resultado}'
+
+    def checar_resultado(self: object, resposta: int) -> bool:
+        if self.resultado == resposta:
+            return True
+        else:
+            return False
+
+    def gerar_operacao(self: object) -> str:
+        op: str = str
+        if self.__operacao == 1:
+            op = '+'
+        elif self.__operacao == 2:
+            op = '-'
+        else:
+            op = '*'
+
+        return f'{self.__valor1} {op} {self.__valor2} = '
+
+
+def ranking():
+    ordem: list = sorted(Jogador.jogadores, key=lambda x: x['tentativas'])
+    print("O Ranking é: \n")
+
+    for x in range(0, len(ordem)):
+        print(f"{x + 1}º Colocado(a): {ordem[x]['nome']}\n")
+
+
+class Jogador:
+    jogadores = []
+
+    def __init__(self: object, nome: str, tentativas: int) -> None:
+        self.__nome: str = nome
+        self.__tentativas: int = tentativas
+        Jogador.jogadores.append({'nome': str(self.__nome), 'tentativas': self.__tentativas})
+        ranking()
